8e879668fcd03fdd47b5a07d413538cf0a6c5f6d
aleks10e <hag20052016@gmail.com>
2024-05-28 20:09:42 +0300

    uprade: cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,7 @@
 import prompt
 
+
 def welcome_user():
     print("Welcome to the Brain Games!")
-    name = prompt.string(f"May I have your name? ")
+    name = prompt.string("May I have your name?")
     print(f'Hello, {name}!')
