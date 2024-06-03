b1e3c5977870ff8e0bb3bbce35f45400f68a1116
aleks10e <hag20052016@gmail.com>
2024-06-03 21:02:33 +0300

    upgrade: engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,18 +4,18 @@ ROUNDS_COUNT = 3
 
 
 def run_game(game):
-    print("Welcome to the Brain Games!")
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
+    print(f'Hello, {name}!')
     print(game.RULE)
 
     for _ in range(ROUNDS_COUNT):
         question, correct_answer = game.generate_question_and_answer()
-        print(f"Question: {question}")
+        print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
 
         if answer == correct_answer:
-            print("Correct!")
+            print('Correct!')
         else:
             print(
                 f"'{answer}' is wrong answer ;(. "
@@ -24,4 +24,4 @@ def run_game(game):
             print(f"Let's try again, {name}!")
             return
 
-    print(f"Congratulations, {name}!")
+    print(f'Congratulations, {name}!')
