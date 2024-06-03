8b6599219c612a83b2b27b0eabf44f8cf4be6b51
aleks10e <hag20052016@gmail.com>
2024-06-03 19:29:01 +0300

    upgrade: prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,7 +1,8 @@
 import random
 
 RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-
+COUNT_FROM = 1
+COUNT_BEFORE = 100
 
 def is_prime(num):
     if num < 2:
@@ -13,7 +14,7 @@ def is_prime(num):
 
 
 def generate_question_and_answer():
-    number = random.randint(1, 100)
+    number = random.randint(COUNT_FROM, COUNT_BEFORE)
     question = str(number)
     correct_answer = 'yes' if is_prime(number) else 'no'
     return question, correct_answer
