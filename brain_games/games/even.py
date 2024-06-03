966b6655273cfaf2540b51b6d40f24d8d1d26056
aleks10e <hag20052016@gmail.com>
2024-06-03 21:03:11 +0300

    upgrade: even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,8 +4,13 @@ RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 COUNT_FROM = 1
 COUNT_BEFORE = 100
 
+
+def is_even(num):
+    return num % 2 == 0
+
+
 def generate_question_and_answer():
     number = random.randint(COUNT_FROM, COUNT_BEFORE)
     question = str(number)
-    correct_answer = 'yes' if number % 2 == 0 else 'no'
+    correct_answer = 'yes' if is_even(number) else 'no'
     return question, correct_answer
