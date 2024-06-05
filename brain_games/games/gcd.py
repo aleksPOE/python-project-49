14766418ee1bbc9a2f3421d381ef0697239866d7
aleks10e <hag20052016@gmail.com>
2024-06-05 19:54:28 +0300

    upgrade: gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -2,13 +2,13 @@ import random
 import math
 
 RULE = 'Find the greatest common divisor of given numbers.'
-COUNT_FROM = 1
-COUNT_BEFORE = 100
+MIN_NUM = 1
+MAX_NUM = 100
 
 
 def generate_question_and_answer():
-    number_1 = random.randint(COUNT_FROM, COUNT_BEFORE)
-    number_2 = random.randint(COUNT_FROM, COUNT_BEFORE)
+    number_1 = random.randint(MIN_NUM, MAX_NUM)
+    number_2 = random.randint(MIN_NUM, MAX_NUM)
     question = f'{number_1} {number_2}'
     answer = str(math.gcd(number_1, number_2))
     return question, answer
