5bfdfe1d9f778e626ab49ec27db0ecc7247809cc
aleks10e <hag20052016@gmail.com>
2024-06-05 19:55:16 +0300

    upgrade: progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,12 +1,15 @@
 import random
 
 RULE = 'What number is missing in the progression?'
+LENGTH_MIN = 5
+START_STEP_MIN = 1
+LENGTH_START_STEP_MAX = 10
 
 
 def generate_question_and_answer():
-    length = random.randint(5, 10)
-    start = random.randint(1, 10)
-    step = random.randint(1, 10)
+    length = random.randint(LENGTH_MIN, LENGTH_START_STEP_MAX)
+    start = random.randint(START_STEP_MIN, LENGTH_START_STEP_MAX)
+    step = random.randint(START_STEP_MIN, LENGTH_START_STEP_MAX)
     progression = [start + step * i for i in range(length)]
     missing_index = random.randint(0, length - 1)
     correct_answer = str(progression[missing_index])
